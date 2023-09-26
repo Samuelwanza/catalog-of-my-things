62410627ff8f43f2c8e40e8621317eae59af8a07
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-26 17:08:23 +0300

    Implement Book class with initialize

---
+++ classes/book.rb
@@ -0,0 +1,15 @@
+require_relative './item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(_source, publish_date, publisher, cover_state)
+    super(_source, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || cover_state.downcase == 'bad'
+  end
+end
