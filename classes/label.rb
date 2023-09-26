9a1491af97aa2157a93ee6726cfe4a1b8dacc231
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-26 17:21:37 +0300

    Implement label class initialize and fix the linters

--- classes/label.rb
+++ classes/label.rb
@@ -1,13 +1,13 @@
 class Label
-    attr_accessor :name, :item
+  attr_accessor :name, :item
 
-    def initialize(name)
-      @name = name
-      @item = nil  # Initialize item as nil
-    end
-
-    def associate_item(item)
-      @item = item
-      item.labels << self
-    end
+  def initialize(name)
+    @name = name
+    @item = nil # Initialize item as nil
   end
+
+  def associate_item(item)
+    @item = item
+    item.labels << self
+  end
+end
